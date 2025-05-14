05131ca0f3dd235d4b181d15da1769b65e800f5f
sabita-yahya <bimantorosabitayahya@gmail.com>
2025-05-14 20:27:29 +0700

    update latian soal dari GPT hehehe

--- study-materials/modulo-loop.js
+++ study-materials/modulo-loop.js
@@ -1,44 +1,44 @@
 // latihan soal looping
 
-for (let i = 1; i <= 100; i++) {
-  if (i % 28 === 0) {
-    console.log("FOURSEVEN");
-  } else if (i % 4 === 0) {
-    console.log("FOUR");
-  } else if (i % 7 === 0) {
-    console.log("SEVEN");
-  } else {
-    console.log(i);
-  }
-}
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 28 === 0) {
+//     console.log("FOURSEVEN");
+//   } else if (i % 4 === 0) {
+//     console.log("FOUR");
+//   } else if (i % 7 === 0) {
+//     console.log("SEVEN");
+//   } else {
+//     console.log(i);
+//   }
+// }
 
-// looping while di dalam for
+// // looping while di dalam for
 
-for (let i = 1; i <= 3; i++) {
-  console.log(`Iterasi ke-${i}`);
+// for (let i = 1; i <= 3; i++) {
+//   console.log(`Iterasi ke-${i}`);
 
-  let j = 1;
-  while (j <= 2) {
-    console.log(`  Sub-iterasi ke-${j}`);
-    j++;
-  }
-}
+//   let j = 1;
+//   while (j <= 2) {
+//     console.log(`  Sub-iterasi ke-${j}`);
+//     j++;
+//   }
+// }
 
-// Latihan Soal Nested Loop
+// // Latihan Soal Nested Loop
 
-for (i = 2; i <= 100; i++) {
-  let isPrime = true;
+// for (i = 2; i <= 100; i++) {
+//   let isPrime = true;
 
-  for (j = 2; j < 1; j++) {
-    if (j % 2 === 0) {
-      isPrime = false;
-      break;
-    }
-  }
-  if (isPrime) {
-    console.log(i);
-  }
-}
+//   for (j = 2; j < 1; j++) {
+//     if (j % 2 === 0) {
+//       isPrime = false;
+//       break;
+//     }
+//   }
+//   if (isPrime) {
+//     console.log(i);
+//   }
+// }
 
 // Penjelasan Detail:
 // Loop Utama (i):
@@ -57,3 +57,43 @@ for (i = 2; i <= 100; i++) {
 // Cek isPrime:
 // Jika isPrime tetap true setelah loop dalam selesai, berarti i adalah bilangan prima.
 // Tampilkan i menggunakan console.log.
+
+
+
+// Latihan 5 Soal looping dari GPT
+// dari yang basic sampai lumayan mikir
+
+// 1. tampilkan angka 1 sampai 10 menggunakan for loop.
+for (let i = 1; i <= 10; i++) {
+  console.log(i)
+}
+// 2. Tampilkan angka dari 1 sampai 20, tapi hanya yang genap saja.
+for (let i = 2; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+// 3. Buatlah pola persegi 5x5 menggunakan karakter *.
+for (let i = 1; i <= 5; i++) {
+  let bintang = "";
+  for (let j = 1; j <= 5; j++) {
+    bintang += "*";
+  }
+  console.log(bintang);
+}
+// 4. Buatlah segitiga siku-siku menggunakan karakter # dengan tinggi 5 baris.
+for (let i = 1; i <= 5; i++) {
+  let pagar = "";
+  for (let j = 1; j <= i; j++) {
+    pagar += "#"; 
+  }
+  console.log(pagar);
+}
+// 5. Tampilkan segitiga angka dengan pola berikut (tinggi 5 baris).
+for (let i = 1; i <= 5; i++) {
+  let angka = "";
+  for (let j = 1; j <= i; j++) {
+    angka += j; 
+  }
+  console.log(angka);
+}
